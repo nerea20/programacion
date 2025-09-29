69e416689cafb92b8ba4a4f202032490930f201a
nerea20 <nereagsdaw@gmail.com>
2025-09-29 09:05:37 +0200

    aquí subo mis primeros ejercicios de programación

---
+++ Ejercicio1.java
@@ -0,0 +1,15 @@
+/*
+ * Descripción: realizar un programa que imprima por pantalla "Este es mi primer programa Java"
+ * Autora: Nerea
+ * Fecha: 24/09/2025
+ */
+package ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Este es mi primer programa Java"); //Muestro por pantalla el mensaje "Este es mi primer programa Java
+
+	}
+
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+/*
+ * Descripción: realizar un programa que imprima por pantalla los siguientes datos
+ * Autora: Nerea
+ * Fecha: 24/09/2025
+ */
+package ejercicio2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Desarrollo de aplicaciones WEB/Multiplataforma"); //imprimo por pantalla los datos que me pide el ejercicio
+		System.out.println("1DAW / DAM");
+		System.out.println("Módulos: ");
+		System.out.println("PRO");
+		System.out.println("SIS");
+
+	}
+
+}
